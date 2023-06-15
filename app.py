8eb6f7d65da8d7e2826c9880a5e3732edc00705b
fajri <fajrimaulana402@gmail.com>
2023-06-14 20:46:18 -0700

    commit kamis 14-06-2023

--- app.py
+++ app.py
@@ -9,19 +9,17 @@ def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
-
 def search():
     query = request.form['query']
-    query = preprocess(query)
     keys = {"query": query}
-    prediction = r.get("https://9a12-36-67-214-133.ngrok-free.app/lr-hs-recommendations", params=keys)
+    prediction = r.get("https://08fd-202-137-230-7.ngrok-free.app/lr-hs-recommendations", params=keys)
     results = prediction.json()
     limited_iterator = itertools.islice(iter(results.items()),10)
     item_list = list(limited_iterator)
 
     print('Query: ', query)
 
-    return render_template('results.html', query=query, result=item_list)
+    return render_template('index.html', query=query, result=item_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
